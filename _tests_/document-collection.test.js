7697a89c48fe8699d0e91f8f6470d4f29ba06146
Abbey Masters <abbeykmasters@gmail.com>
2019-09-19 12:58:48 -0700

    working on get all function

--- _tests_/document-collection.test.js
+++ _tests_/document-collection.test.js
@@ -7,21 +7,22 @@ jest.mock('../lib/files.js', () => ({
 // for setting up mock expectations
 const { readFile, writeFile, readdir } = require('../lib/files');
 const DocumentCollection = require('../lib/document-collection');
-const collection = new DocumentCollection;
+const collection = new DocumentCollection('document');
 
 describe('Document Collection', () => {
+  
+  const exampleObject = {
+    name: 'abbey'
+  };
+
   it('saves file', () => {
     //arrange
-    const exampleObject = {
-      name: 'abbey'
-    };
-    
     writeFile.mockResolvedValue(exampleObject);
     
     //act
     return collection.save(exampleObject)
     .then(() => {
-          const dest = `./${exampleObject.id}.json`;
+          const dest = `./document/${exampleObject.id}.json`;
           const writeCalls = writeFile.mock.calls;
           expect(writeCalls.length).toBe(1);
           expect(writeCalls[0][0]).toBe(dest);
@@ -39,6 +40,35 @@ describe('Document Collection', () => {
     collection.save({})
       .catch(err => {
         expect(err).toBe(error);
-      });
-  });
+      });      
+    });
+
+    // getting ID test and error
+
+    it('get id', () => {
+      //arrange
+      const source = `./document/${exampleObject.id}.json`;
+      readFile.mockResolvedValue(exampleObject);
+
+      //act
+      return collection.get(exampleObject.id)
+        .then(() => {
+          const readCalls = readFile.mock.calls;
+          expect(readCalls.length).toBe(1); 
+          expect(readCalls[0][0]).toBe(source);
+        });
+    });
+  
+    it('get id — propagates error', () => {
+      //arrange
+      const error = 'file error';
+      writeFile.mockRejectedValueOnce(error);
+      expect.assertions(0);
+  
+      //act
+      collection.save({})
+        .catch(err => {
+          expect(err).toBe(error);
+        });
+    });
 });
