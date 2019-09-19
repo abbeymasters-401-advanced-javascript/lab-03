084de413fd52643d5e1daf9a1eea7988e2efdd10
Abbey Masters <abbeykmasters@gmail.com>
2019-09-19 09:57:18 -0700

    one save test

--- _tests_/document-collection.test.js
+++ _tests_/document-collection.test.js
@@ -6,7 +6,26 @@ jest.mock('../lib/files.js', () => ({
 
 // for setting up mock expectations
 const { readFile, writeFile, readdir } = require('../lib/files');
+const DocumentCollection = require('../lib/document-collection');
+const collection = new DocumentCollection;
 
 describe('Document Collection', () => {
-  // TODO
+  it('saves file', () => {
+    //arrange
+      const dest = './id.json';
+      const exampleObject = {
+        name: 'abbey'
+      };
+
+      writeFile.mockResolvedValue(exampleObject);
+
+    //act
+      return collection.save(exampleObject)
+        .then(() => {
+          const writeCalls = writeFile.mock.calls;
+          expect(writeCalls.length).toBe(1);
+          expect(writeCalls[0][0]).toBe(dest);
+          expect(writeCalls[0][1]).toBe(JSON.stringify(exampleObject));
+        });
+  });
 });
